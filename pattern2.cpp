835ccb8dc5c0bf1555be4173ce5448d0e2517bc2
Vivek Kumar jha <67068290+vivekjha1213@users.noreply.github.com>
2022-12-01 09:05:41 +0530

    Add files via upload

---
+++ pattern2.cpp
@@ -0,0 +1,123 @@
+// k// #include<iostream>
+// // using namespace std;
+// // int main(){
+
+// //  cout<<"Enter a number for print pattern \n";
+// //  int n;
+// //  cin>>n;
+// //  for (int i = 1; i <=n; i++)
+// //  {
+// //      for (int  j = 1; j <= i; j++)
+     
+      
+// //         cout<<"*";
+     
+     
+// //      cout<<"\n";
+// //  }
+ 
+
+// //     return (0);
+
+
+
+// // }// #include<iostream>
+// // using namespace std;
+// // int main(){
+
+// //  cout<<"Enter a number for print pattern \n";
+// //  int n;
+// //  cin>>n;
+// //  for (int i = 1; i <=n; i++)
+// //  {
+// //      for (int  j = 1; j <= i; j++)
+     
+      
+// //         cout<<"*";
+     
+     
+// //      cout<<"\n";
+// //  }
+ 
+
+// //     return (0);
+
+
+
+// // }// #include<iostream>
+// // using namespace std;
+// // int main(){
+
+// //  cout<<"Enter a number for print pattern \n";
+// //  int n;
+// //  cin>>n;
+// //  for (int i = 1; i <=n; i++)
+// //  {
+// //      for (int  j = 1; j <= i; j++)
+     
+      
+// //         cout<<"*";
+     
+     
+// //      cout<<"\n";
+// //  }
+ 
+
+// //     return (0);
+
+
+
+// // }// #include<iostream>
+// // using namespace std;
+// // int main(){
+
+// //  cout<<"Enter a number for print pattern \n";
+// //  int n;
+// //  cin>>n;
+// //  for (int i = 1; i <=n; i++)
+// //  {
+// //      for (int  j = 1; j <= i; j++)
+     
+      
+// //         cout<<"*";
+     
+     
+// //      cout<<"\n";
+// //  }
+ 
+
+// //     return (0);
+
+
+
+// // }
+
+#include<iostream>
+using namespace std;
+int main(){
+
+ cout<<"Enter a number for print pattern \n";
+ int n;
+ cin>>n;
+ cout<<"\n";
+ for (int i = 1; i <=n; i++)
+ {
+     for (int  j = 1; j <= i; j++)
+     
+      
+        cout<<j;
+     
+     
+     cout<<"\n";
+ }
+ 
+
+    return (0);
+
+
+
+}
+
+
+
+
